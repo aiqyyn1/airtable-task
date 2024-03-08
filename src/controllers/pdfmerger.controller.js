59019756828119ebe07a9470fd9a77dc1c8c42b6
aidoha <ibrayevaiqyn@gmail.com>
2024-03-09 01:35:23 +0500

    feat:add tables in pdf

--- src/controllers/pdfmerger.controller.js
+++ src/controllers/pdfmerger.controller.js
@@ -61,19 +61,63 @@ async function mergeAndModifyPDFs(pdfUrls) {
     const pages = await mergedPdf.copyPages(pdfDoc, pdfDoc.getPageIndices());
     pages.forEach((page, index) => {
       const { width, height } = page.getSize();
-      const text = `Qadamloft ${index + 1}`;
-      page.drawText(text, {
-        x: 50,
-        y: height - 50,
+      const modifiedPage = mergedPdf.addPage(page);
+
+      // Define table properties
+      const tableX = 50;
+      const tableY = height - 150;
+      const cellWidth = 100;
+      const cellHeight = 30;
+      const tableGap = 10;
+
+      // Draw table header
+      modifiedPage.drawText('Header 1', {
+        x: tableX,
+        y: tableY,
         font: helveticaFont,
         size: 12,
+        color: rgb(0, 0, 0),
       });
 
-      mergedPdf.addPage(page);
+      modifiedPage.drawText('Header 2', {
+        x: tableX + cellWidth + tableGap,
+        y: tableY,
+        font: helveticaFont,
+        size: 12,
+        color: rgb(0, 0, 0),
+      });
+
+      modifiedPage.drawText('Header 3', {
+        x: tableX + 2 * (cellWidth + tableGap),
+        y: tableY,
+        font: helveticaFont,
+        size: 12,
+        color: rgb(0, 0, 0),
+      });
+
+      // Draw table content
+      const tableContent = [
+        ['Cell 1', 'Cell 2', 'Cell 3'],
+        ['Cell 4', 'Cell 5', 'Cell 6'],
+        // Add more rows as needed
+      ];
+
+      tableContent.forEach((row, rowIndex) => {
+        row.forEach((cell, colIndex) => {
+          modifiedPage.drawText(cell, {
+            x: tableX + colIndex * (cellWidth + tableGap),
+            y: tableY - (rowIndex + 1) * (cellHeight + tableGap),
+            font: helveticaFont,
+            size: 10,
+            color: rgb(0, 0, 0),
+          });
+        });
+      });
     });
   }
 
   return await mergedPdf.save();
 }
+
 
 module.exports = pdfMergerController;
