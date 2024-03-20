5e93cb63fb2af803bffe23035494ba3cfbfebd70
Qadamloft <76691527+qadam711@users.noreply.github.com>
2024-03-20 15:14:26 +0600

    Update avr.js
    TODO task for refactoring

--- src/routes/avr.js
+++ src/routes/avr.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
 const getFirsController = require('../controllers/avr.controller');
+
+//TODO: refactor code - rename "first controller" to "avr_rus" 
+
 router.get('/blanks1', getFirsController)
 
 module.exports = router
