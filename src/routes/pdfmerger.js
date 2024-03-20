15fa660bc68ea8c627bb6fffc2704a0d330d4362
Qadamloft <76691527+qadam711@users.noreply.github.com>
2024-03-20 15:13:34 +0600

    Update pdfmerger.js
    TODO task to rename controller

--- src/routes/pdfmerger.js
+++ src/routes/pdfmerger.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
 const getFirsController = require('../controllers/pdfmerger.controller');
+/* TODO: refactor code - rename "first controller and pdfmerger.controller" 
+to "blank zakazov controller" 
+*/
 router.get('/pdfmerger', getFirsController)
 
 module.exports = router
