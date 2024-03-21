74052695440ac03897980cc7848fa8f85208bdd5
aidoha <ibrayevaiqyn@gmail.com>
2024-03-21 14:14:16 +0500

    fix/pdfmerger

--- src/controllers/pdfmerger.controller.js
+++ src/controllers/pdfmerger.controller.js
@@ -3,12 +3,6 @@ const { PDFDocument, rgb, degrees, StandardFonts } = require('pdf-lib');
 const fontkit = require('@pdf-lib/fontkit');
 const { base } = require('../../airtable');
 
-const tbl_tapsyrys_header = 'заказы общее';
-const tbl_tapsyrys_lines = 'заказы подробно';
-const col_chertezh = 'чертеж';
-const col_client_name = 'Аты (from клиент)';
-const col_client_phone = 'тел2 (from клиент)';
-
 async function pdfMergerController(req, res) {
   const recordID = req.query.recordID;
 
@@ -29,17 +23,18 @@ async function pdfMergerController(req, res) {
 
 async function fetchData(recordID) {
   let pdfUrls = [];
+  const zakazy_podrobno = 'заказы подробно';
   return new Promise((resolve, reject) => {
-    base(tbl_tapsyrys_lines)
+    base(zakazy_podrobno)
       .select({
-        view: 'Aikyn', //No need view name Aikyn
+        view: 'Aikyn',
         filterByFormula: `{record_id (from заказ номер)} = '${recordID}'`,
       })
       .eachPage(
         function page(records, fetchNextPage) {
           try {
             records.forEach((item) => {
-              if (item.get(chertezh)) pdfUrls.push(item.get(chertezh)[0].url);
+              if (item.get('чертеж')) pdfUrls.push(item.get('чертеж')[0].url);
             });
             fetchNextPage();
           } catch (e) {
@@ -56,17 +51,93 @@ async function fetchData(recordID) {
       );
   });
 }
-findRecord = (recordID) => {
+
+function findRecord(recordID) {
+  const zakazy_obwee = 'заказы общее';
   return new Promise((resolve, reject) => {
-    base(tbl_tapsyrys_header).find(recordID, (err, record) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(record);
-      }
-    });
+    base(zakazy_obwee)
+      .select({
+        view: 'Aikyn1 чертеж',
+        filterByFormula: `{record_id} = '${recordID}'`,
+      })
+      .eachPage(function page(records, fetchNextPage) {
+        resolve(records); // Resolve inside the callback
+        fetchNextPage();
+      })
+      .catch((err) => {
+        reject(err); // Handle rejection here
+      });
   });
-};
+}
+
+function tapsyrysZholdary(recordID) {
+  const zakazy_podrobno = 'заказы подробно';
+  let data = [];
+  return new Promise((resolve, reject) => {
+    base(zakazy_podrobno)
+      .select({
+        view: 'Aikyn чертеж',
+        filterByFormula: `{record_id (from заказ номер)} = '${recordID}'`,
+      })
+      .eachPage(function page(records, fetchNextPage) {
+        try {
+          records.forEach((item) => {
+            const n = item.get('№');
+            const naimenovanie = item.get('Наименование1');
+            const kol_vo = item.get('Кол-во');
+            const postavshik = item.get('поставшик');
+
+            const kraska_metal = item.get('краска метал');
+            const url = item.get('чертеж');
+
+            data.push({
+              n: n,
+              naimenovanie: naimenovanie,
+              kol_vo: kol_vo,
+              postavshik: String(postavshik),
+              kraska_metal: kraska_metal,
+              url: url && url[0].url,
+            });
+          });
+          fetchNextPage();
+        } catch (e) {
+          reject(e); // Reject if an error occurs during processing
+        }
+      })
+      .then(() => {
+        resolve(data); // Resolve with the data after processing is complete
+      })
+      .catch((error) => {
+        reject(error); // Reject if an error occurs during querying
+      });
+  });
+}
+function dostavkaData(recordID) {
+  const dostavka = 'доставки';
+  return new Promise((resolve, reject) => {
+    base(dostavka)
+      .select({
+        view: 'Aikyn чертеж',
+        filterByFormula: `{record_id (from заказ)} = '${recordID}'`,
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          try {
+            console.log('==>', dostavka);
+            resolve(records); // Resolve with records
+            fetchNextPage();
+          } catch (e) {
+            reject(e);
+          }
+        },
+        function done(err) {
+          if (err) {
+            reject(err); // Reject if there's an error
+          }
+        }
+      );
+  });
+}
 
 async function mergeAndModifyPDFs(pdfUrls, recordID) {
   const mergedPdf = await PDFDocument.create();
@@ -78,40 +149,84 @@ async function mergeAndModifyPDFs(pdfUrls, recordID) {
   const customFont = await mergedPdf.embedFont(fontBytes);
 
   const data = await findRecord(recordID);
-  const aty_from_client = 'Аты' + ' ' + String(data.get(client_name));
-  const tel2_from_client = 'Тел' + ' ' + String(data.get(client_phone));
+  const aikyn_chertezh = await tapsyrysZholdary(recordID);
+  const dostavka = await dostavkaData(recordID);
+  // console.log(dostavka);
+  //dostavka data
+  // const address = dostavka.get('адрес');
+  // const kol_vo_reisov = dostavka.get('кол-во рейсов');
+  // const vygruzka = dostavka.get('выгрузка');
+  // const ustanovka = dostavka.get('установка');
+  // const komment = dostavka.get('комментарий');
+  // console.log('==>', address, kol_vo_reisov, vygruzka, ustanovka, komment);
+  // zakazy obwee data
+  const nomer = String(data[0].get('номер'));
+  const manager = String(data[0].get('Менеджер'));
+  const srochno = String(data[0].get('Срочно'));
+  const aty_from_client = 'Аты' + ' ' + String(data[0].get('Аты (from клиент)'));
+  const tel2_from_client = 'Тел' + ' ' + String(data[0].get('тел2 (from клиент)'));
+
+  const fontSize = 16;
   for (const pdfUrl of pdfUrls) {
     const pdfBytes = await fetch(pdfUrl).then((res) => res.arrayBuffer());
     const pdfDoc = await PDFDocument.load(pdfBytes);
 
     const pages = await mergedPdf.copyPages(pdfDoc, pdfDoc.getPageIndices());
-    pages.forEach((page, index) => {
-      const { width, height } = page.getSize();
-      const modifiedPage = mergedPdf.addPage(page);
 
-      const fontSize = 12;
+    pages[0].drawText(aty_from_client, {
+      x: 50,
+      y: 510,
+      size: fontSize,
+      font: customFont,
+      color: rgb(0, 0, 0, 0),
+    });
+    pages[0].drawText(tel2_from_client, {
+      x: 50,
+      y: 520,
+      size: fontSize,
+      font: customFont,
+      color: rgb(0, 0, 0, 0),
+    });
+    pages[0].drawText(manager, {
+      x: 50,
+      y: 530,
+      size: fontSize,
+      font: customFont,
+      color: rgb(0, 0, 0, 0),
+    });
+    pages[0].drawText(nomer, {
+      x: 50,
+      y: 540,
+      size: fontSize,
+      font: customFont,
+      color: rgb(0, 0, 0, 0),
+    });
+    pages[0].drawText(srochno, {
+      x: 50,
+      y: 550,
+      size: fontSize,
+      font: customFont,
+      color: rgb(0, 0, 0, 0),
+    });
 
-      // Add a new page after each PDF file
-      if (index < pages.length - 1) {
-        const newPage = mergedPdf.addPage([width, height]);
-        // const textXNewPage = (newPage.getWidth() - widthHeight) / 2;
-        // const textYNewPage = (newPage.getHeight() - textHeight) / 2;
-
-        newPage.drawText(aty_from_client, {
+    // Вставьте этот код для добавления данных из aikyn_chertezh на страницу
+    aikyn_chertezh.forEach((dataItem, index) => {
+      if (index < pages.length) {
+        // Убедимся, что есть достаточно страниц для добавления данных
+        const textToDraw = `${dataItem.n}. ${dataItem.naimenovanie}, ${dataItem.kol_vo}, ${dataItem.postavshik}, ${dataItem.kraska_metal}`;
+        pages[index].drawText(textToDraw, {
           x: 50,
-          y: 510,
-          size: fontSize,
-          font: customFont,
-          color: rgb(0, 0, 0, 0), // Black color
-        });
-        newPage.drawText(tel2_from_client, {
-          x: 50,
-          y: 520,
+          y: 480 - index * 20,
           size: fontSize,
           font: customFont,
           color: rgb(0, 0, 0, 0),
         });
       }
+    });
+    pages.forEach((page, index) => {
+      const { width, height } = page.getSize();
+      const modifiedPage = mergedPdf.addPage(page);
+      // Add a new page after each PDF file
     });
   }
 
