5114a510a958ffcd6356832974a57a7dc9ee7932
aidoha <ibrayevaiqyn@gmail.com>
2024-03-27 23:16:31 +0500

    feat:add other things

--- src/controllers/pdfmerger.controller.js
+++ src/controllers/pdfmerger.controller.js
@@ -27,7 +27,6 @@ async function fetchData(recordID) {
   return new Promise((resolve, reject) => {
     base(zakazy_podrobno)
       .select({
-        view: 'Aikyn',
         filterByFormula: `{record_id (from заказ номер)} = '${recordID}'`,
       })
       .eachPage(
@@ -57,7 +56,6 @@ function findRecord(recordID) {
   return new Promise((resolve, reject) => {
     base(zakazy_obwee)
       .select({
-        view: 'Aikyn1 чертеж',
         filterByFormula: `{record_id} = '${recordID}'`,
       })
       .eachPage(function page(records, fetchNextPage) {
@@ -72,32 +70,39 @@ function findRecord(recordID) {
 
 function tapsyrysZholdary(recordID) {
   const zakazy_podrobno = 'заказы подробно';
+  const chertezh = 'чертеж';
   let data = [];
   return new Promise((resolve, reject) => {
     base(zakazy_podrobno)
       .select({
-        view: 'Aikyn чертеж',
         filterByFormula: `{record_id (from заказ номер)} = '${recordID}'`,
       })
       .eachPage(function page(records, fetchNextPage) {
         try {
           records.forEach((item) => {
-            const n = item.get('№');
-            const naimenovanie = item.get('Наименование1');
-            const kol_vo = item.get('Кол-во');
-            const postavshik = item.get('поставшик');
+            if (item.get(chertezh)) {
+              const n = item.get('№');
+              const naimenovanie = item.get('Наименование1');
+              const kol_vo = item.get('Кол-во');
+              const client_from_zakaz = item.get('клиент (from заказ номер)');
+              const tel1 = item.get('тел1');
+              const postavshik = item.get('поставшик');
+              const nomer = item.get('номер');
+              const data_zdachi = item.get('дата сдачи на товар');
+              const kraska_metal = item.get('краска метал');
 
-            const kraska_metal = item.get('краска метал');
-            const url = item.get('чертеж');
-
-            data.push({
-              n: n,
-              naimenovanie: naimenovanie,
-              kol_vo: kol_vo,
-              postavshik: String(postavshik),
-              kraska_metal: kraska_metal,
-              url: url && url[0].url,
-            });
+              data.push({
+                n: n,
+                naimenovanie: naimenovanie,
+                kol_vo: kol_vo,
+                postavshik: String(postavshik),
+                kraska_metal: kraska_metal,
+                nomer: nomer,
+                client_from_zakaz: client_from_zakaz,
+                tel1: tel1,
+                data_zdachi: data_zdachi,
+              });
+            }
           });
           fetchNextPage();
         } catch (e) {
@@ -117,7 +122,6 @@ function dostavkaData(recordID) {
   return new Promise((resolve, reject) => {
     base(dostavka)
       .select({
-        view: 'Aikyn чертеж',
         filterByFormula: `{record_id (from заказ)} = '${recordID}'`,
       })
       .eachPage(
@@ -150,97 +154,120 @@ async function mergeAndModifyPDFs(pdfUrls, recordID) {
   const data = await findRecord(recordID);
   const aikyn_chertezh = await tapsyrysZholdary(recordID);
   const dostavka = await dostavkaData(recordID);
-  //dostavka data
-
   const address = dostavka[0].get('адрес');
   const kol_vo_reisov = dostavka[0].get('кол-во рейсов');
   const vygruzka = dostavka[0].get('выгрузка');
   const ustanovka = dostavka[0].get('установка');
   const komment = dostavka[0].get('комментарий');
-
   const nomer = String(data[0].get('номер'));
   const manager = String(data[0].get('Менеджер'));
   const srochno = String(data[0].get('Срочно'));
-  const aty_from_client = 'Аты' + ' ' + String(data[0].get('Аты (from клиент)'));
-  const tel2_from_client = 'Тел' + ' ' + String(data[0].get('тел2 (from клиент)'));
+  const nomer_zakaza = 'Номер заказ:' + ' ' + nomer;
+  const manager_zakaza = 'Менеджер:' + ' ' + manager;
+  const aty_from_client = 'Аты:' + ' ' + String(data[0].get('Аты (from клиент)'));
+  const tel2_from_client = 'Тел:' + ' ' + String(data[0].get('тел2 (from клиент)'));
+  const srochno_zakaza = 'Срочно:' + ' ' + srochno;
   const fontSize = 16;
   for (const pdfUrl of pdfUrls) {
     const pdfBytes = await fetch(pdfUrl).then((res) => res.arrayBuffer());
     const pdfDoc = await PDFDocument.load(pdfBytes);
 
     const pages = await mergedPdf.copyPages(pdfDoc, pdfDoc.getPageIndices());
-    pages[0].drawText(aty_from_client, {
+    pages[0].drawText(nomer_zakaza, {
       x: 300,
       y: 560,
       size: fontSize,
       font: customFont,
       color: rgb(0, 0, 0, 0),
     });
-    pages[0].drawText(tel2_from_client, {
+    pages[0].drawText(aty_from_client, {
       x: 300,
       y: 540,
       size: fontSize,
       font: customFont,
       color: rgb(0, 0, 0, 0),
     });
-    pages[0].drawText(manager, {
+    pages[0].drawText(tel2_from_client, {
       x: 300,
       y: 520,
       size: fontSize,
       font: customFont,
       color: rgb(0, 0, 0, 0),
     });
-
-    pages[0].drawText(address, {
+    pages[0].drawText(manager_zakaza, {
       x: 300,
-      y: 540,
+      y: 500,
+      size: fontSize,
+      font: customFont,
+      color: rgb(0, 0, 0, 0),
+    });
+    pages[0].drawText(srochno_zakaza, {
+      x: 300,
+      y: 480,
+      size: fontSize,
+      font: customFont,
+      color: rgb(0, 0, 0, 0),
+    });
+    let size = 0;
+    aikyn_chertezh.forEach((item, index) => {
+      const chertezh_podrobno = `${item.n}   ${item.naimenovanie}  ${item.kol_vo}   ${item.postavshik}  ${item.kraska_metal}`;
+      size += index * 20;
+      pages[0].drawText(chertezh_podrobno, {
+        x: 300,
+        y: 460 - index * 20,
+        size: fontSize,
+        font: customFont,
+        color: rgb(0, 0, 0, 0),
+      });
+    });
+    pages[0].drawText(address, {
+      x: 320,
+      y: size - 20,
       size: fontSize,
       font: customFont,
       color: rgb(0, 0, 0, 0),
     });
     pages[0].drawText(kol_vo_reisov ? vygruzka : '', {
-      x: 50,
-      y: 550,
+      x: 320,
+      y: size - 20,
       size: fontSize,
       font: customFont,
       color: rgb(0, 0, 0, 0),
     });
     pages[0].drawText(vygruzka ? vygruzka : '', {
-      x: 50,
-      y: 510,
+      x: 320,
+      y: size - 20,
       size: fontSize,
       font: customFont,
       color: rgb(0, 0, 0, 0),
     });
     pages[0].drawText(ustanovka ? ustanovka : '', {
-      x: 50,
-      y: 520,
+      x: 320,
+      y: size - 20,
       size: fontSize,
       font: customFont,
       color: rgb(0, 0, 0, 0),
     });
     pages[0].drawText(komment ? ustanovka : '', {
-      x: 50,
-      y: 530,
+      x: 320,
+      y: size - 20,
       size: fontSize,
       font: customFont,
       color: rgb(0, 0, 0, 0),
     });
+    // aikyn_chertezh.forEach((item, index) => {
+    //   if (index < pages.length) {
+    //     const chertezh_podrobno = `${item.n} ${item.nomer}  ${item.client_from_zakaz}  ${item.tel1}  ${item.naimenovanie}  ${item.kol_vo}  ${item.data_zdachi}  ${item.postavshik}  ${item.kraska_metal} `;
 
-    // Вставьте этот код для добавления данных из aikyn_chertezh на страницу
-    aikyn_chertezh.forEach((dataItem, index) => {
-      if (index < pages.length) {
-        // Убедимся, что есть достаточно страниц для добавления данных
-        const textToDraw = `${dataItem.n}. ${dataItem.naimenovanie}, ${dataItem.kol_vo}, ${dataItem.postavshik}, ${dataItem.kraska_metal}`;
-        pages[index].drawText(textToDraw, {
-          x: 50,
-          y: 480 - index * 20,
-          size: fontSize,
-          font: customFont,
-          color: rgb(0, 0, 0, 0),
-        });
-      }
-    });
+    //     pages[index].drawText(chertezh_podrobno, {
+    //       x: 300,
+    //       y: 480 - index * 20,
+    //       size: fontSize,
+    //       font: customFont,
+    //       color: rgb(0, 0, 0, 0),
+    //     });
+    //   }
+    // });
     pages.forEach((page, index) => {
       const { width, height } = page.getSize();
       const modifiedPage = mergedPdf.addPage(page);
