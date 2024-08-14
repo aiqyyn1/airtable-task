1dd896c566f8654333a95ca41c3b091e2f936f3a
aidoha <ibrayevaiqyn@gmail.com>
2024-08-14 14:47:59 +0500

    decompose:functions

--- src/routes/avr.js
+++ src/routes/avr.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const {getSecondController} = require('../controllers/avr.controller');
 
-//TODO: refactor code - rename "first controller" to "avr_rus"
+
 
 router.get('/avr_rus_print', getSecondController);
 
--- src/routes/avr_rus_scan.js
+++ src/routes/avr_rus_scan.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {getSecondController} = require('../controllers/avr.controller');
+const { getSecondController } = require('../controllers/avr.controller');
 router.get('/avr_rus_scan', getSecondController);
 
 module.exports = router;
