a437cf1a92980864222b332d74727d1d1a8d785a
aidoha <ibrayevaiqyn@gmail.com>
2024-08-14 14:22:37 +0500

    decompose:functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -80,4 +80,122 @@ function deliverData(recordID) {
       );
   });
 }
-module.exports = { findRecord, fetchRecords, deliverData };
+async function fetchData(recordID) {
+  let items = [];
+  const zakazy_podrobno = 'Сатылым2';
+
+  return new Promise((resolve, reject) => {
+    base(zakazy_podrobno)
+      .select({
+        filterByFormula: `{record_id (from заказ номер)} = '${recordID}'`,
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          try {
+            records.forEach((item) => {
+              const n = item.get('№');
+              const url = item.get('сызба') ? item.get('сызба')[0].url : null;
+              if (url) {
+                items.push({ n, url });
+              }
+            });
+            fetchNextPage();
+          } catch (e) {
+            reject(e);
+          }
+        },
+        function done(err) {
+          if (err) {
+            reject(err);
+          } else {
+            const sortedUrls = items.sort((a, b) => a.n - b.n).map((item) => item.url);
+            resolve(sortedUrls);
+          }
+        }
+      );
+  });
+}
+
+function tapsyrysZholdary(recordID) {
+  const zakazy_podrobno = 'Сатылым2';
+  let data = [];
+  return new Promise((resolve, reject) => {
+    base(zakazy_podrobno)
+      .select({
+        filterByFormula: `{record_id (from заказ номер)} = '${recordID}'`,
+      })
+      .eachPage(function page(records, fetchNextPage) {
+        try {
+          records.forEach((item) => {
+            data.push({
+              n: item.get('№'),
+              naimenovanie: item.get('ТауарАты1'),
+              kol_vo: item.get('Саны'),
+              postavshik: item.get('поставшик'),
+              kraska_metal: item.get('металл бояу'),
+              client_from_zakaz: item.get('клиент (from заказ номер)'),
+              tel1: item.get('тел1'),
+              data_zdachi: item.get('дата сдачи на товар'),
+              designer: item.get('дизайнер'),
+              cenaDostavki: item.get('цена (доставки)'),
+              nomer_zakaza: item.get('номер заказа'),
+            });
+          });
+          fetchNextPage();
+        } catch (e) {
+          reject(e);
+        }
+      })
+      .then(() => {
+        data.sort((a, b) => a.n - b.n);
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+function tapsyrysZholdary1(recordID) {
+  const zakazy_podrobno = 'Сатылым2';
+  let data = [];
+  return new Promise((resolve, reject) => {
+    base(zakazy_podrobno)
+      .select({
+        filterByFormula: `{record_id (from заказ номер)} = '${recordID}'`,
+      })
+      .eachPage(function page(records, fetchNextPage) {
+        try {
+          records.forEach((item) => {
+            if (item.get('сызба')) {
+              data.push({
+                n: item.get('№'),
+                naimenovanie: item.get('ТауарАты1'),
+                kol_vo: item.get('Саны'),
+                postavshik: item.get('поставшик'),
+                kraska_metal: item.get('металл бояу'),
+                client_from_zakaz: item.get('клиент (from заказ номер)'),
+                tel1: item.get('тел1'),
+                data_zdachi: item.get('дата сдачи на товар'),
+                designer: item.get('дизайнер'),
+                cenaDostavki: item.get('цена (доставки)'),
+                nomer_zakaza: item.get('номер заказа'),
+              });
+            }
+          });
+          fetchNextPage();
+        } catch (e) {
+          reject(e);
+        }
+      })
+      .then(() => {
+        data.sort((a, b) => a.n - b.n);
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+module.exports = { findRecord, fetchRecords, deliverData, fetchData, tapsyrysZholdary, tapsyrysZholdary1};
