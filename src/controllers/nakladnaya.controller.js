84d4b29d451c86a05f9b452a96c5bf79b280f7f4
aidoha <ibrayevaiqyn@gmail.com>
2024-09-21 16:01:36 +0500

    refactor:code

---
+++ src/controllers/nakladnaya.controller.js
@@ -0,0 +1,89 @@
+const {
+  findRecord,
+  convertDate,
+  fetchNakladanaya,
+  fetchSatylym2,
+  numToWordsRU,
+} = require('../utils/utils');
+const { path, pdf, ejs } = require('../../airtable');
+const nakladnayaController = async (req, res) => {
+  const recordID = req.query.recordID;
+
+  try {
+    const records = await findRecord(recordID);
+
+    // const documents = await getDocuments(recordID);
+    const satylym2 = await fetchNakladanaya(recordID);
+    console.log(satylym2)
+    let itogoSum = 0;
+    satylym2.forEach((item) => {
+      itogoSum += item.kol_vo;
+    });
+
+    const nomer_zakaza = await fetchSatylym2(recordID);
+    const nomer_nak = records[0].get('номер Нак');
+    const data_nak = records[0].get('дата Нак');
+    const ip = records[0].get('ИП имя (from ИП)');
+    const biin = records[0].get('БИН (from ИП)');
+    const nazvanie_firmy_3 = records[0].get('название фирмы 3');
+    const rukovaditel = records[0].get('руководитель (from ИП)');
+    const itogo = records[0].get('Жиынтық сомасы');
+    const filename =
+      String(nomer_zakaza[0].get('номер заказа')) + '-' + nazvanie_firmy_3 + '-' + 'АЖА' + '.pdf';
+    let airtableData = {
+      nomer_nak: nomer_nak[0],
+      data_nak: convertDate(data_nak[0]),
+      ip: ip[0],
+      biin: biin,
+      esf: satylym2,
+      nazvanie_firmy_3,
+      rukovaditel: rukovaditel,
+      itogo: itogo,
+      propisItogo: numToWordsRU(itogo),
+      itogoSum: itogoSum,
+    };
+    const templatePath = path.resolve(__dirname, '../views/nakladnaya/nakladnaya.ejs');
+    ejs.renderFile(templatePath, { reportdata: airtableData }, (err, data) => {
+      if (err) {
+        console.log(err, 'Error in rendering template');
+        res.status(500).send('Error in rendering template');
+      } else {
+        const options = {
+          format: 'A4',
+          orientation: 'landscape',
+
+          base: 'file:///' + __dirname,
+
+          header: {
+            height: '2mm',
+          },
+
+          footer: {
+            height: '20mm',
+          },
+        };
+
+        pdf.create(data, options).toFile(filename, function (err, data) {
+          if (err) {
+            console.log('Error creating PDF ' + err);
+            res.status(500).send('Error creating PDF');
+          } else {
+            console.log('PDF created successfully:', data);
+            res.download(filename, function (err) {
+              if (err) {
+                console.log('Error during file download:', err);
+                res.status(500).send('Error during file download');
+              } else {
+                console.log('File downloaded successfully');
+              }
+            });
+          }
+        });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server error');
+  }
+};
+module.exports = { nakladnayaController };
